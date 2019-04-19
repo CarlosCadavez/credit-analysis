6488cb6664c219c4f28db4c21c31c2723674278d
Carlos Cadavez <cs.cadavez@gmail.com>
2019-04-19 10:40:30 -0300

    improving credit propose resource to add location

--- src/main/java/br/com/calcard/credit/resource/CreditProposalResource.java
+++ src/main/java/br/com/calcard/credit/resource/CreditProposalResource.java
@@ -9,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.calcard.credit.domain.CreditProposal;
 import br.com.calcard.credit.repository.CreditProposals;
@@ -39,8 +41,16 @@ public class CreditProposalResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<List<CreditProposal>> addCreditPropose() throws URISyntaxException {
-		return ResponseEntity.created(new URI("")).build();
+	public ResponseEntity<List<CreditProposal>> addCreditPropose(@RequestBody CreditProposal creditPropose) throws URISyntaxException {
+		CreditProposal creditProposal = creditProposals.save(creditPropose);
+		
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(creditProposal.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
