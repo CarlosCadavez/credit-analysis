802016016295255f017534ffe042cd93f5e8a2d1
Carlos Cadavez <cs.cadavez@gmail.com>
2019-04-19 00:39:21 -0300

    creating options to enuns

--- src/main/java/br/com/calcard/credit/domain/MaritalStatus.java
+++ src/main/java/br/com/calcard/credit/domain/MaritalStatus.java
@@ -1,5 +1,20 @@
 package br.com.calcard.credit.domain;
 
 public enum MaritalStatus {
+	
+	MARRIED("Casado(a)"),
+	SINGLE("Solteiro(a)"),
+	DIVORCED("Divorciado(a)"),
+	WIDOWED("Viúvo(a)");
+	
+	private String relationship;
+	
+	MaritalStatus(String relationship) {
+		this.relationship = relationship;
+	}
+	
+	public String getRelationshipDescription() {
+		return relationship;
+	}
 
 }
