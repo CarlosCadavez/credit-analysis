cc058c54add27a4d6e306319348311a0e85796c6
Carlos Cadavez <cs.cadavez@gmail.com>
2019-04-19 10:09:05 -0300

    created an endpoint post to add credit propose

--- src/main/java/br/com/calcard/credit/resource/CreditProposalResource.java
+++ src/main/java/br/com/calcard/credit/resource/CreditProposalResource.java
@@ -1,18 +1,20 @@
 package br.com.calcard.credit.resource;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.calcard.credit.domain.CreditProposal;
 import br.com.calcard.credit.repository.CreditProposals;
 import br.com.calcard.credit.service.CreditProposalService;
-import br.com.calcard.credit.service.exception.CreditProposeNotFoundedException;
 
 @RestController
 @RequestMapping(CreditProposalResource.PATH_RESOURCE)
@@ -32,9 +34,13 @@ public class CreditProposalResource {
 	}
 	
 	@GetMapping(value = "/{cpf}")
-	public ResponseEntity<CreditProposal> creditProposalById(@PathVariable("cpf") String cpf) {
+	public ResponseEntity<CreditProposal> creditProposalByCpfNumber(@PathVariable("cpf") String cpf) {
 		return ResponseEntity.ok(creditProposalService.findByCpf(cpf));
-		
+	}
+	
+	@PostMapping
+	public ResponseEntity<List<CreditProposal>> addCreditPropose() throws URISyntaxException {
+		return ResponseEntity.created(new URI("")).build();
 	}
 
 }
